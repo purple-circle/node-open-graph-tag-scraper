cd567e52aa81cd2383172b87b1e170c54fc02a44
Juha Tauriainen <juha@bin.fi>
2015-05-01 13:30:41 -0700

    Fixed package.json test script

--- test/server.js
+++ test/server.js
@@ -8,5 +8,5 @@ app.use('/', express.static("../examples/"));
 var server = app.listen(3000, function () {
   var port = server.address().port;
 
-  console.log('Open browser in http://localhost:%s/', port);
+  console.log('Test server running');
 });
